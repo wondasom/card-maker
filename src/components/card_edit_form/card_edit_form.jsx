56213af0971b73187b8c99cba8165f3215521f33
dasomwon <wondasom93@gmail.com>
2020-10-04 22:54:09 +0200

    fix switch option not working

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -3,102 +3,105 @@ import styles from './card_edit_form.module.css';
 import Button from '../button/button';
 
 const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
-	const nameRef = useRef();
-	const companyRef = useRef();
-	const titleRef = useRef();
-	const emailRef = useRef();
-	const messageRef = useRef();
-	const themeRef = useRef();
-	const {
-		name,
-		company,
-		title,
-		email,
-		message,
-		theme,
-		fileName,
-		fileURL
-	} = card;
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
-	const onFileChange = (file) => {
-		updateCard({
-			...card,
-			fileName: file.name,
-			fileURL: file.url
-		});
-	};
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
 
-	const onChange = (event) => {
-		if (event.currentTarget == null) {
-			return;
-		}
-		event.preventDefault();
-		updateCard({
-			...card,
-			[event.currentTarget.name]: event.currentTarget.value
-		});
-	};
-	const onSubmit = () => {
-		deleteCard(card);
-	};
-	return (
-		<form className={styles.form}>
-			<input
-				ref={nameRef}
-				className={styles.input}
-				type='text'
-				name='name'
-				value={name}
-				onChange={onChange}
-			/>
-			<input
-				ref={companyRef}
-				className={styles.input}
-				type='text'
-				name='company'
-				value={company}
-				onChange={onChange}
-			/>
-			<select
-				ref={themeRef}
-				className={styles.select}
-				name='theme'
-				value={theme}
-				onChange={onChange}
-			>
-				<option value='light'>light</option>
-				<option value='dark'>dark</option>
-				<option value='colorful'>colorful</option>
-			</select>
-			<input
-				ref={titleRef}
-				className={styles.input}
-				type='text'
-				name='title'
-				value={title}
-				onChange={onChange}
-			/>
-			<input
-				ref={emailRef}
-				className={styles.input}
-				type='text'
-				name='email'
-				value={email}
-				onChange={onChange}
-			/>
-			<textarea
-				ref={messageRef}
-				className={styles.textarea}
-				name='message'
-				value={message}
-				onChange={onChange}
-			/>
-			<div className={styles.fileInput}>
-				<FileInput name={fileName} onFileChange={onFileChange} />
-			</div>
-			<Button name='Delete' onClick={onSubmit} />
-		</form>
-	);
+  const onFileChange = file => {
+    updateCard({
+      ...card,
+      fileName: file.name,
+      fileURL: file.url,
+    });
+  };
+
+  const onChange = event => {
+    if (event.currentTarget == null) {
+      return;
+    }
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  const onSubmit = () => {
+    deleteCard(card);
+  };
+
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        ref={nameRef}
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        ref={companyRef}
+        value={company}
+        onChange={onChange}
+      />
+      <select
+        className={styles.select}
+        name="theme"
+        ref={themeRef}
+        value={theme}
+        onChange={onChange}
+      >
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        ref={titleRef}
+        value={title}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        ref={emailRef}
+        value={email}
+        onChange={onChange}
+      />
+      <textarea
+        className={styles.textarea}
+        ref={messageRef}
+        name="message"
+        value={message}
+        onChange={onChange}
+      />
+      <div className={styles.fileInput}>
+        <FileInput name={name} onFileChange={onFileChange} />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
+  );
 };
 
 export default CardEditForm;
