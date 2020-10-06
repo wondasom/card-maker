58c4ffdabf22505c6b2fd1e0e2bc83e9fa51e6ef
dasomwon <wondasom93@gmail.com>
2020-10-06 23:14:29 +0200

    add a github button

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,10 +4,21 @@ import styles from './header.module.css';
 const Header = memo(({ onLogout }) => (
 	<header className={styles.header}>
 		{onLogout && (
-			<button className={styles.logout} onClick={onLogout}>
-				Logout
-			</button>
+			<>
+				<button className={styles.logout} onClick={onLogout}>
+					Logout
+				</button>
+				<a
+					href='https://github.com/wondasom/card-maker'
+					target='_blank'
+					rel='noopener noreferrer'
+					className={styles.github}
+				>
+					Go to Github
+				</a>
+			</>
 		)}
+
 		<img className={styles.logo} src='/images/logo.png' alt='logo'></img>
 		<h1 className={styles.title}>Business Card Maker</h1>
 	</header>
